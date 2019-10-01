ab073f59b298d503f4d98be02e43c0ffd36118e7
david-dd-amar <David@CVMED-C02TG17GG8WL.local>
2019-09-30 22:02:29 -0700

    automatic load from bucket

--- tools/gcp_functions.R
+++ tools/gcp_functions.R
@@ -25,6 +25,20 @@ load_from_bucket<-function(file,bucket,delete=T){
   return(res)
 }
 
+download_bucket_files_to_local_dir<-function(bucket,local_path=NULL){
+  if(is.null(local_path)){local_path=paste(getwd(),"gs_download",sep="/")}
+  # download files
+  # remove old data if exists
+  if(file.exists(local_path)){system(paste("rm -r",local_path))}
+  # download
+  system(paste("mkdir",local_path))
+  cmd = paste("~/google-cloud-sdk/bin/gsutil -m cp -r",paste(bucket,"*",sep=""),local_path)
+  system(cmd)
+  # get the data
+  downloaded_files = list.files(local_path,full.names = T)
+  return(list(downloaded_files=downloaded_files,local_path=local_path))
+}
+
 save_to_bucket<-function(...,file,bucket){
   save(...,file = file)
   system(paste("~/google-cloud-sdk/bin/gsutil cp",
@@ -59,7 +73,7 @@ read_metabolomics_datasets_from_release_bucket<-function(bucket,local_path=NULL,
       names(platforms_in_bucket) = gsub("/",replacement = "",names(platforms_in_bucket))
       for(platform in names(platforms_in_bucket)){
         platform_bucket = platforms_in_bucket[platform]
-        platform_bucket = paste(platform_bucket,"PROCESSED/",sep="")
+        platform_bucket = get_files_in_bucket(platform_bucket)[1]
         platform_datasets = get_files_in_bucket(platform_bucket)
         named_bucket = paste(platform_bucket,"NAMED/",sep="")
         named_data = read_single_metabolomics_dataset(named_bucket)
@@ -78,30 +92,24 @@ read_metabolomics_datasets_from_release_bucket<-function(bucket,local_path=NULL,
       }
     }
   }
+  return(metabolomics_parsed_datasets)
 }
-# test
-bucket = "gs://motrpac-internal-release1-1-results/metabolomics/"
+# # test
+# bucket = "gs://motrpac-internal-release1-1-results/metabolomics/"
 
 #' Read the data from the release
 #' @description read the data of a single folder in the data release; remove data rows that are all NAs or missing
 #' @description assumes that bucket has exactly four files: experimental details, sample metadata, metabolite metadata, and the dataset called the results file
 read_single_metabolomics_dataset<-function(bucket,local_path=NULL,delete=T){
-  if(is.null(local_path)){local_path=paste(getwd(),"gs_download",sep="/")}
-  # download files
-  # remove old data if exists
-  if(file.exists(local_path)){system(paste("rm -r",local_path))}
-  # download
-  system(paste("mkdir",local_path))
-  cmd = paste("~/google-cloud-sdk/bin/gsutil -m cp -r",paste(bucket,"*",sep=""),local_path)
-  system(cmd)
-  # get the data
-  downloaded_files = list.files(local_path,full.names = T)
+  download_bucket = download_bucket_files_to_local_dir(bucket,local_path)
+  local_path = download_bucket[[2]]
+  downloaded_files = download_bucket[[1]]
   downloaded_files = downloaded_files[!(
     grepl("experimental",downloaded_files,ignore.case = T) &
       grepl("details",downloaded_files,ignore.case = T))]
   sample_meta_file = downloaded_files[
     grepl("metadata",downloaded_files,ignore.case = T) & 
-      grepl("_sample",downloaded_files)]
+      grepl("sample",downloaded_files)]
   downloaded_files = setdiff(downloaded_files,sample_meta_file)
   results_file = downloaded_files[grepl("results",downloaded_files,ignore.case = T)]
   downloaded_files = setdiff(downloaded_files,results_file)
@@ -208,15 +216,93 @@ merge_metabolomics_datasets<-function(named_data,unnamed_data){
   return(res)
 }
 
-# test
-bucket1 = "gs://motrpac-internal-release1-1-results/metabolomics/gtech/T68/RPNEG/PROCESSED/UNNAMED/"
-unnamed_data = read_single_metabolomics_dataset(bucket1)
-bucket2 = "gs://motrpac-internal-release1-1-results/metabolomics/gtech/T68/RPNEG/PROCESSED/NAMED/"
-named_data = read_single_metabolomics_dataset(bucket2)
-merged_data = merge_metabolomics_datasets(named_data,unnamed_data)
-boxplot(log(1+merged_data$control_data))
+# # test
+# bucket1 = "gs://motrpac-internal-release1-1-results/metabolomics/gtech/T68/RPNEG/PROCESSED/UNNAMED/"
+# unnamed_data = read_single_metabolomics_dataset(bucket1)
+# bucket2 = "gs://motrpac-internal-release1-1-results/metabolomics/gtech/T68/RPNEG/PROCESSED/NAMED/"
+# named_data = read_single_metabolomics_dataset(bucket2)
+# merged_data = merge_metabolomics_datasets(named_data,unnamed_data)
+# boxplot(log(1+merged_data$control_data))
 
-read_proteomics_datasets_from_release_bucket<-function(bucket){
-  
+read_proteomics_datasets_from_release_bucket<-function(bucket,local_path=NULL,delete=T){
+  proteomics_parsed_datasets = list()
+  site_buckets = get_files_in_bucket(bucket)
+  names(site_buckets) = gsub(bucket,replacement = "",site_buckets)
+  names(site_buckets) = gsub("/",replacement = "",names(site_buckets))
+  for(site in names(site_buckets)){
+    site_bucket = site_buckets[site]
+    tissues_in_bucket = get_files_in_bucket(site_bucket)
+    tissues_in_bucket = tissues_in_bucket[!grepl("qc_report",tissues_in_bucket)]
+    names(tissues_in_bucket) = gsub(site_bucket,replacement = "",tissues_in_bucket)
+    names(tissues_in_bucket) = gsub("/",replacement = "",names(tissues_in_bucket))
+    for(tissue in names(tissues_in_bucket)){
+      tissue_bucket = tissues_in_bucket[tissue]
+      platforms_in_bucket = get_files_in_bucket(tissue_bucket)
+      names(platforms_in_bucket) = gsub(tissue_bucket,replacement = "",platforms_in_bucket)
+      names(platforms_in_bucket) = gsub("/",replacement = "",names(platforms_in_bucket))
+      for(platform in names(platforms_in_bucket)){
+        platform_bucket = platforms_in_bucket[platform]
+        platform_bucket = get_files_in_bucket(platform_bucket)[1]
+        print(length(get_files_in_bucket(platform_bucket)))
+        dataset_name = paste(c(site,tissue,platform),collapse=",")
+        dataset = read_single_proteomics_dataset(platform_bucket)
+        proteomics_parsed_datasets[[dataset_name]] = dataset
+        print(dataset_name)
+        print(dim(dataset$ratio_data))
+      }
+    }
+  }
+  return(proteomics_parsed_datasets)
 }
+# # test
+# bucket = "gs://motrpac-internal-release1-1-results/proteomics"
+# save_to_bucket(proteomics_parsed_datasets,file="proteomics_parsed_datasets.RData",
+#                bucket = "gs://bic_data_analysis/pass1a/")
+
+read_single_proteomics_dataset<-function(bucket,local_path=NULL,delete=T){
+  download_bucket = download_bucket_files_to_local_dir(bucket,local_path)
+  local_path = download_bucket[[2]]
+  downloaded_files = download_bucket[[1]]
+  
+  # ratio data
+  l1 = read_proteomics_subdataset_by_regex(downloaded_files,"ratio")
+  if(!is.null(l1)){names(l1) = paste("ratio",names(l1),sep="_")}
+  # intensity data
+  l2 = read_proteomics_subdataset_by_regex(downloaded_files,"rii")
+  if(!is.null(l2)){names(l2) = paste("rii",names(l2),sep="_")}
+  
+  if(delete){system(paste("rm -r",local_path))}
+  
+  merged = c(l1,l2)
+  return(merged)
+}
+
+read_proteomics_subdataset_by_regex<-function(downloaded_files,regex="ratio"){
+  data_file = downloaded_files[grepl(regex,downloaded_files) &
+                                 grepl("results",downloaded_files)]
+  metadata_file = downloaded_files[grepl("vial",downloaded_files) &
+                                     grepl("metadata",downloaded_files)]
+  if(length(metadata_file)==0){
+    metadata_file = downloaded_files[grepl("metadata",downloaded_files)]
+  }
+  
+  data = fread(data_file,data.table = F,stringsAsFactors = F,header = T)
+  row_annot_cols = which(sapply(data,class)!="numeric")
+  row_annot = as.matrix(data[,row_annot_cols])
+  data = data[,-row_annot_cols]
+  
+  sample_meta = fread(metadata_file,data.table = F,stringsAsFactors = F,header = T)
+  rownames(sample_meta) = sample_meta[,1]
+  if(!all(is.element(colnames(data)[-c(1:2)],set=rownames(sample_meta)))){
+    print("Error in ratio data, not all samples are in the metadata file")
+    return(NULL)
+  }
+  return(list(data=data,sample_meta=sample_meta,row_annot=row_annot))
+}
+# #test
+# bucket =  "gs://motrpac-internal-release1-1-results/proteomics/broad/T58/ac/results/"
+
+
+
+
 
